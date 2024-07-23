31cf63fb511f32e089a4a9b009b53fa82d97d355
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2024-07-23 12:43:06 +0330

    grid-wrap

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
@@ -11,15 +12,15 @@ import (
 
 func main() {
 	myApp := app.New()
-	myWindow := myApp.NewWindow("Box Layout")
+	myWindow := myApp.NewWindow("Grid Wrap Layout")
 
-	text1 := canvas.NewText("Hello", color.White)
-	text2 := canvas.NewText("There", color.White)
-	text3 := canvas.NewText("(right)", color.White)
-
-	grid := container.New(layout.NewGridLayout(3), text1, text2, text3)
-
+	text1 := canvas.NewText("1", color.White)
+	text2 := canvas.NewText("2", color.White)
+	text3 := canvas.NewText("3", color.White)
+	grid := container.New(layout.NewGridWrapLayout(fyne.NewSize(50, 50)),
+		text1, text2, text3)
 	myWindow.SetContent(grid)
-	myWindow.ShowAndRun()
 
+	// myWindow.Resize(fyne.NewSize(180, 75))
+	myWindow.ShowAndRun()
 }
