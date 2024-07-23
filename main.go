2e5646f0c6768da3a0f9536a6257b93ea787d91f
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2024-07-23 12:41:06 +0330

    grid

--- main.go
+++ main.go
@@ -3,21 +3,23 @@ package main
 import (
 	"image/color"
 
-	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 )
 
 func main() {
-
 	myApp := app.New()
+	myWindow := myApp.NewWindow("Box Layout")
 
-	mywindow := myApp.NewWindow("master")
+	text1 := canvas.NewText("Hello", color.White)
+	text2 := canvas.NewText("There", color.White)
+	text3 := canvas.NewText("(right)", color.White)
 
-	rect := canvas.NewRectangle(color.Opaque)
-	mywindow.SetContent(rect)
+	grid := container.New(layout.NewGridLayout(3), text1, text2, text3)
 
-	mywindow.Resize(fyne.NewSize(500, 500))
-	mywindow.ShowAndRun()
+	myWindow.SetContent(grid)
+	myWindow.ShowAndRun()
 
 }
