03a530e00f971a843ce7fba5bcfed34f634d4fac
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2024-07-29 09:21:20 +0330

    set icon

--- main.go
+++ main.go
@@ -2,31 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	a := app.New()
+	a := app.NewWithID("com.example.myapp")
 
-	// بارگذاری آیکون از فایل
-	iconPath := "./icon.png"
-	iconResource, err := fyne.LoadResourceFromPath(iconPath)
-	if err != nil {
-		fmt.Printf("خطا در بارگذاری آیکون از مسیر %s: %v\n", iconPath, err)
-		return
-	}
+	// Use one of the default icons provided by fyne
+	iconResource := theme.FyneLogo()
 
-	// تنظیم آیکون برای برنامه
+	// Set the icon for the application
 	a.SetIcon(iconResource)
+	fmt.Println("Icon set for the application")
 
-	// ایجاد یک پنجره جدید
+	// Create a new window
 	w := a.NewWindow("My Fyne App")
 
-	// تنظیم محتوای پنجره
+	// Set the icon for the window
+	w.SetIcon(iconResource)
+	fmt.Println("Icon set for the window")
+
+	// Set the content of the window
 	w.SetContent(container.NewVBox(
 		widget.NewLabel("Hello Fyne!"),
 		widget.NewButton("Quit", func() {
@@ -34,6 +36,18 @@ func main() {
 		}),
 	))
 
-	// نمایش و اجرای برنامه
+	// Ensure the dock icon is set on macOS
+	if fyne.CurrentDevice().IsMobile() == false {
+		if desk, ok := a.(interface {
+			SetSystemTrayIcon(fyne.Resource)
+		}); ok {
+			desk.SetSystemTrayIcon(iconResource)
+		}
+	}
+
+	// Set the environment variable for MacOS app bundle
+	os.Setenv("FYNE_DESKTOP", "1")
+
+	// Show and run the application
 	w.ShowAndRun()
 }
