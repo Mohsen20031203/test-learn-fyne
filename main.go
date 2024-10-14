0aa9b95b484f385880a462d12224634d7d85ec85
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2024-10-14 14:59:14 +0330

    badger

--- main.go
+++ main.go
@@ -2,30 +2,58 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 
-	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/dgraph-io/badger/v4"
 )
 
 func main() {
-	dbPath := "path/to/your/database"
+	opt := badger.DefaultOptions("test")
+	db, err := badger.Open(opt)
+	fmt.Println("true")
+	if err != nil {
+		return
+	}
+	defer db.Close()
 
-	// بررسی وجود فایل دیتابیس
-	if _, err := os.Stat("/Users/macbookpro/Desktop/helo"); os.IsNotExist(err) {
-		// اگر دیتابیس وجود ندارد، آن را ایجاد کنید
-		db, err := leveldb.OpenFile(dbPath, nil)
-		if err != nil {
-			log.Fatalf("Failed to create database: %v", err)
+	// اضافه کردن 100 کلید/مقدار به دیتابیس
+	err = db.Update(func(txn *badger.Txn) error {
+		for i := 0; i < 100; i++ {
+			key := fmt.Sprintf("key%d", i)
+			value := fmt.Sprintf("value%d", i)
+			err := txn.Set([]byte(key), []byte(value))
+			if err != nil {
+				return err
+			}
+			fmt.Println("Inserted:", key)
 		}
-		defer db.Close()
+		return nil
+	})
+	if err != nil {
+		return
+	}
 
-		fmt.Println("Database created successfully.")
-	} else {
-		// اگر دیتابیس وجود دارد، کار دیگری انجام دهید
-		fmt.Println("Database already exists. Performing other actions.")
+	// پیمایش (iteration) داده‌ها از دیتابیس
+	err = db.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchSize = 1 // تنظیم میزان پیش‌بارگذاری (prefetching)
+		it := txn.NewIterator(opts)
+		defer it.Close()
 
-		// کد مورد نیاز برای انجام کار دیگر
-		// ...
+		fmt.Println("Iterating through keys and values:")
+		for it.Rewind(); it.Valid(); it.Next() {
+			item := it.Item()
+			k := item.Key()
+			err := item.Value(func(v []byte) error {
+				fmt.Printf("Key: %s, Value: %s\n", k, v)
+				return nil
+			})
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return
 	}
 }
